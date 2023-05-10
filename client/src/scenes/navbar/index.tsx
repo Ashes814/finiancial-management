6b1fac1a2089dba945ea504b66e056c01bf948a4
Ashes814 <1534661514@qq.com>
2023-05-10 20:27:54 +0800

    connect database

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -5,9 +5,8 @@ import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 
 // 定义props的类型
-type Props = {};
 
-const Navbar = (props: Props) => {
+const Navbar = () => {
   const { palette } = useTheme();
 
   // 判断当前位于哪个页面 dashboard or prediction
